32d1f91bb51b30dc17affcc458dc52a251874dfc
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-23 21:59:17 +0200

    Unfinished sprint

--- GDC-project/Assets/ManScript/ManMove.cs
+++ GDC-project/Assets/ManScript/ManMove.cs
@@ -4,30 +4,56 @@ using UnityEngine;
 
 public class ManMove : MonoBehaviour
 {
+    public float speed = 0.4F;
+    public float turnSpeed = 1;
+
+    public bool Issprinting = false;
+    public float sprintingMultiplier;
+
+    Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        public float speed = 0.3
-        public float turnSpeed = 0.3
-        
-        Rigidbody rb;
-}
+        rb = GetComponent<Rigidbody>();
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        ManMove();
-
-    }    void ManMove()
-    {
-    float moveInput = moveInput.GetAxis("Vertical");//Variable for move input Så dude kan Går rund.
-    float turnInput = moveInput.GetAxis("Horizontal");
-
-    float moveSpeed = speed * moveInput;
-
-    Vector3 newVelocity = transform.forward * moveSpeed;
-    newVelocity.y = rb.velocity.y;
-
-
+        MoveHandler();
     }
+    void MoveHandler()
+    {
+        float moveInput = Input.GetAxis("Vertical");//Variable for move input
+        float turnInput = Input.GetAxis("Horizontal");
+
+        float moveSpeed = speed * moveInput;
+
+        Vector3 newVelocity = transform.forward * moveSpeed;
+        newVelocity.y = rb.velocity.y;
+
+        gameObject.GetComponent<Transform>().Rotate(Vector3.up * turnInput * turnSpeed); // Rotates the tank around Y-axis
+
+        rb.velocity = newVelocity; //Sets velocity of the tank to movespeed in the forward direction
+        
+        if (Input.GetKey(KeyCode.LeftShift))//sprint button
+        {
+            Issprinting = true;
+
+        }
+        else
+        {
+            Issprinting = false;
+        }
+
+        if (Issprinting == true)
+        {
+            moveInput *= sprintingMultiplier;
+        }
+    }
+
+}
 
